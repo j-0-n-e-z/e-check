dbba3a740f17934d095b0cb9dff5dc902cc14c82
j-0-n-e-z <j000neeez@gmail.com>
2023-09-07 17:49:03 +0500

    :tada: danger modal close on outside click

--- client/src/components/DangerModal/DangerModal.tsx
+++ client/src/components/DangerModal/DangerModal.tsx
@@ -1,14 +1,15 @@
-import type { FC } from 'react'
+import { useRef, type FC } from 'react'
 
 import type { Additive } from '@/common'
+import { useClickOutside } from '@/hooks'
+import { dangerLevelTexts } from '@/src/data/dangerLevelTexts'
 
-import { getDangerLevelInText } from '../Additives/components/helpers/getDangerText'
+import { XMark } from './XMark'
 
 interface DangerModalProps {
   additive: Additive
   close: () => void
 }
-// TODO: make modal close on outside click
 export const DangerModal: FC<DangerModalProps> = ({
   additive: {
     danger: { reasons, level },
@@ -16,22 +17,29 @@ export const DangerModal: FC<DangerModalProps> = ({
     name
   },
   close
-}) => (
-  <div className='fixed left-0 top-0 grid min-h-screen w-screen place-items-center bg-black/[0.5]'>
-    <div className='flex w-1/2 flex-col gap-y-3 rounded-md bg-whity p-6 text-dark'>
-      <div className='flex justify-between'>
-        <h3 className='text-lg font-bold'>
-          {code} ({name}) - {getDangerLevelInText(level)}
-        </h3>
-        <button onClick={close}>close</button>
-      </div>
-      <div>
-        {reasons.length ? (
-          reasons.map((r, i) => <p key={i}>{r}</p>)
-        ) : (
-          <p>Причина опасноти {code} неизвестна.</p>
-        )}
+}) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+  useClickOutside(modalRef, close)
+
+  return (
+    <div className='fixed left-0 top-0 grid min-h-screen w-screen place-items-center bg-black/[0.5]'>
+      <div ref={modalRef} className='flex w-1/2 flex-col gap-y-3 rounded-md bg-whity p-6 text-dark'>
+        <div className='flex justify-between'>
+          <h3 className='text-lg font-bold'>
+            {code} ({name}) - {dangerLevelTexts[level]}
+          </h3>
+          <button onClick={close}>
+            <XMark />
+          </button>
+        </div>
+        <div>
+          {reasons.length ? (
+            reasons.map((r, i) => <p key={i}>{r}</p>)
+          ) : (
+            <p>Причина опасноти {code} неизвестна.</p>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
