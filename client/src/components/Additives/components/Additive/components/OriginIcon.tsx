c988600fb2b03bd9a1cdade76ea04c65f9da5903
j-0-n-e-z <j000neeez@gmail.com>
2023-09-08 12:55:15 +0500

    :tada: add alt to image

--- client/src/components/Additives/components/Additive/components/OriginIcon.tsx
+++ client/src/components/Additives/components/Additive/components/OriginIcon.tsx
@@ -10,9 +10,9 @@ interface OriginIconProps {
 export const OriginIcon: FC<OriginIconProps> = ({ origin }) => (
   <img
     key={origin}
+    alt='изображение происхождения'
     className='inline h-10 w-10'
     src={`http://localhost:8080/assets/${originIconNames[origin]}.png`}
     title={`${origin} происхождение`}
   />
 )
-
