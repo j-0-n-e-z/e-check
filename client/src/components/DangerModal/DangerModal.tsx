69aedf5fabe4db160a8dd08ec1cb2aaba256edc4
j-0-n-e-z <j000neeez@gmail.com>
2023-09-09 20:05:27 +0500

    :tada: add appear animation for modal

--- client/src/components/DangerModal/DangerModal.tsx
+++ client/src/components/DangerModal/DangerModal.tsx
@@ -1,3 +1,4 @@
+import { motion as m } from 'framer-motion'
 import { useRef, type FC } from 'react'
 
 import type { Additive } from '@/common'
@@ -17,7 +18,12 @@ export const DangerModal: FC<DangerModalProps> = ({ additive, close }) => {
 
   return (
     <div className='fixed left-0 top-0 grid min-h-screen w-screen place-items-center bg-black/[0.5]'>
-      <div ref={modalRef} className='flex w-1/2 flex-col gap-y-3 rounded-md bg-whity p-6 text-dark'>
+      <m.div
+        ref={modalRef}
+        animate={{ opacity: 1, scale: 1 }}
+        className='flex w-1/2 flex-col gap-y-3 rounded-md bg-whity p-6 text-dark'
+        initial={{ opacity: 0, scale: 0.5 }}
+      >
         <div className='flex justify-between'>
           <h3 className='text-lg font-bold'>
             {code} ({name}) - {dangerLevelTexts[danger.level]}
@@ -33,7 +39,7 @@ export const DangerModal: FC<DangerModalProps> = ({ additive, close }) => {
             <p>Причина опасноти {code} неизвестна.</p>
           )}
         </div>
-      </div>
+      </m.div>
     </div>
   )
 }
